aada40982db44d40b0ba2d6b268abc24d8fefc73
FelixCoutinho <felix.coutinho@gmail.com>
2014-09-29 09:55:38 -0300

    Exercício 1

---
+++ src/br/loja/testes/RealizarCompra.java
@@ -0,0 +1,21 @@
+package br.loja.testes;
+
+import br.loja.dominio.Carrinho;
+import br.loja.dominio.Item;
+import br.loja.dominio.Produto;
+
+public class RealizarCompra {
+
+	public static void main(String[] args) {
+		Produto iPhone6 = new Produto("PRD-0001", 2399.99);
+		Produto macBookPro = new Produto("PRD-0009", 7999.99);
+
+		Carrinho carrinhoDeComprasCliente1 = new Carrinho();
+
+		carrinhoDeComprasCliente1.getItens().add(new Item(macBookPro));
+		carrinhoDeComprasCliente1.getItens().add(new Item(iPhone6));
+		
+		System.out.println(carrinhoDeComprasCliente1.getTotal());
+	}
+
+}
