a3b3681155b4967a1b7e54dc87bec778d4351ac8
FelixCoutinho <felix.coutinho@gmail.com>
2014-09-29 11:08:16 -0300

    Refatorando para o jUnit

--- src/br/loja/testes/RealizarCompra.java
+++ src/br/loja/testes/RealizarCompra.java
@@ -1,12 +1,16 @@
 package br.loja.testes;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.loja.dominio.Carrinho;
 import br.loja.dominio.Item;
 import br.loja.dominio.Produto;
 
 public class RealizarCompra {
 
-	public static void main(String[] args) {
+	@Test
+	public void main() {
 		Produto iPhone6 = new Produto("PRD-0001", 2399.99);
 		Produto macBookPro = new Produto("PRD-0009", 7999.99);
 		Produto macMini = new Produto("PRD-0009", 599.99);
@@ -16,13 +20,15 @@ public class RealizarCompra {
 		carrinhoDeComprasCliente1.getItens().add(new Item(macBookPro));
 		carrinhoDeComprasCliente1.getItens().add(new Item(iPhone6));
 		carrinhoDeComprasCliente1.getItens().add(new Item(macMini));
-		
+
 		Carrinho carrinhoDeComprasCliente2 = new Carrinho();
-		
-		System.out.println(carrinhoDeComprasCliente1.getTotal() == 10999.97 ? true : false);
-		System.out.println(carrinhoDeComprasCliente1.isVazio() == false ? true : false);
-		System.out.println(carrinhoDeComprasCliente2.getTotal() == 0.0 ? true : false);
-		System.out.println(carrinhoDeComprasCliente2.isVazio() == true ? true : false);
+
+		Assert.assertEquals(Double.valueOf(10999.97),
+				carrinhoDeComprasCliente1.getTotal());
+		Assert.assertTrue(carrinhoDeComprasCliente1.isVazio());
+		Assert.assertEquals(Double.valueOf(0.0),
+				carrinhoDeComprasCliente2.getTotal());
+		Assert.assertTrue(carrinhoDeComprasCliente2.isVazio());
 	}
 
 }
