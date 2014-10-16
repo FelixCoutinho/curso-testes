727456ff5226176032479e9b8cff9a210341d36c
FelixCoutinho <felix.coutinho@gmail.com>
2014-10-16 09:49:02 -0300

    Exercício 6. Rule com Locale. Simulando erro.

--- test/br/loja/testes/ProdutoTest.java
+++ test/br/loja/testes/ProdutoTest.java
@@ -1,13 +1,20 @@
 package br.loja.testes;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import br.loja.dominio.Produto;
 
 public class ProdutoTest {
-
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void naoDeveAceitarProdutosComValoresNegativos() {
 		new Produto("PRD-0001", -0.99);
+	}
+
+	@Test
+	public void deveriaFormatarPrecoComPadraoBrasileiro() {
+		assertEquals("A formatação do preço não corresponde com o padrão pt-BR.", "19,99",  new Produto("PRD-0001", 19.99).getPreco().toString());
 	}
 }
