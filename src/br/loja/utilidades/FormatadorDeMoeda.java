6a64b94349c814c9f701544a163a90b3435fa03a
FelixCoutinho <felix.coutinho@gmail.com>
2014-10-16 10:12:23 -0300

    Exercício 6. Demonstrando outro problema, se o servidor for en-us.

--- src/br/loja/utilidades/FormatadorDeMoeda.java
+++ src/br/loja/utilidades/FormatadorDeMoeda.java
@@ -2,10 +2,11 @@ package br.loja.utilidades;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class FormatadorDeMoeda {
 
-	private static NumberFormat formatadorPadrao = NumberFormat.getCurrencyInstance();
+	private static NumberFormat formatadorPadrao = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 
 	public static String formatar(BigDecimal valor) {
 		formatadorPadrao.setMinimumFractionDigits(1);
