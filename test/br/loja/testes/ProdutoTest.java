a1f6614ab7a84c5aee2132ce8a22e01f817ec4a1
FelixCoutinho <felix.coutinho@gmail.com>
2014-10-16 09:49:31 -0300

    Exercício 6. Testando com Before e After

--- test/br/loja/testes/ProdutoTest.java
+++ test/br/loja/testes/ProdutoTest.java
@@ -2,12 +2,28 @@ package br.loja.testes;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.loja.dominio.Produto;
 
 public class ProdutoTest {
 	
+	private static final Locale localeOriginal = Locale.getDefault();
+
+	@Before
+	public void setup() {
+		Locale.setDefault(new Locale("pt", "BR"));
+	}
+
+	@After
+	public void cleanUp() {
+		Locale.setDefault(localeOriginal);
+	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void naoDeveAceitarProdutosComValoresNegativos() {
 		new Produto("PRD-0001", -0.99);
