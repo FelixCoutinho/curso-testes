7fac1a80a6562130bf72aced663f3f2d59855288
Felix Coutinho <felix.coutinho@gmail.com>
2014-10-18 20:51:52 -0300

    Exemplo de problema com legibilidade de teste com List.

--- testes/br/loja/utilidades/TestesUtilidades.java
+++ testes/br/loja/utilidades/TestesUtilidades.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestesCalculadora.class, TestesFormatacaoMoeda.class })
+@SuiteClasses({ TestesCalculadora.class, TestesFormatacaoMoeda.class, TestesListaOrdenada.class })
 public class TestesUtilidades {
 
 }
