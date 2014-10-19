d1ebce26cd5284317abb25c63a7914af84cf816f
Felix Coutinho <felix.coutinho@gmail.com>
2014-10-19 08:32:55 -0300

    Corrigindo encoding.

--- testes/br/loja/utilidades/TestesCalculadora.java
+++ testes/br/loja/utilidades/TestesCalculadora.java
@@ -8,19 +8,19 @@ public class TestesCalculadora {
 
 	@Test
 	public void deveriaSomarDoisNumerosInteiros() {
-		assertEquals("A soma n„o foi realizada corretamente.",
+		assertEquals("A soma n√£o foi realizada corretamente.",
 				Integer.valueOf(2), Calculadora.somar(1, 1));
 	}
 	
 	@Test
 	public void deveriaSomarUmComDois(){
-		assertEquals("A soma n„o foi realizada corretamente.",
+		assertEquals("A soma n√£o foi realizada corretamente.",
 				Integer.valueOf(3), Calculadora.somar(1, 2));
 	}
 	
 	@Test
 	public void deveriaSomarTresComDez(){
-		assertEquals("A soma n„o foi realizada corretamente.",
+		assertEquals("A soma n√£o foi realizada corretamente.",
 				Integer.valueOf(13), Calculadora.somar(3, 10));
 	}
 
