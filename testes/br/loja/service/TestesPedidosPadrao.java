87035027445da8210566f7cb28491b97425c4ea9
Felix Coutinho <felix.coutinho@gmail.com>
2014-10-19 22:13:16 -0300

    Exercício 12. Testes do pedido com verificações do tipo Stub.

--- testes/br/loja/service/TestesPedidosPadrao.java
+++ testes/br/loja/service/TestesPedidosPadrao.java
@@ -1,13 +1,16 @@
 package br.loja.service;
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import br.loja.dominio.Pagamento;
@@ -47,10 +50,13 @@ public class TestesPedidosPadrao {
 				.criar();
 		Pagamento pagamento = CriadorDePagamento.umPagamento().autorizado().criar();
 		
-		Mockito.when(this.pagamentoService.pagar(pedido)).thenReturn(pagamento);
-		Mockito.when(this.entregaService.solicitarEntrega(pedido)).thenReturn(pedidoComUidCorreios);
+		when(this.pagamentoService.pagar(pedido)).thenReturn(pagamento);
+		when(this.entregaService.solicitarEntrega(pedido)).thenReturn(pedidoComUidCorreios);
 		
-		Assert.assertThat(this.pedidoServicePadrao.efetuarPedido(pedido).getUidCorreios(), Matchers.notNullValue());
+		assertThat(this.pedidoServicePadrao.efetuarPedido(pedido).getUidCorreios(), notNullValue());
+		
+		verify(this.pagamentoService).pagar(pedido);
+		verify(this.entregaService).solicitarEntrega(pedido);
 	}
 
 	@Test
@@ -58,18 +64,18 @@ public class TestesPedidosPadrao {
 		Pedido pedido = CriadorDePedido.umPedido()
 				.comCarrinho(CriadorDeCarrinho.umCarrinho().comProduto(CriadorDeProduto.umProduto().criar()).criar())
 				.criar();
-		Pedido pedidoComUidCorreios = CriadorDePedido.umPedido()
-				.comCarrinho(CriadorDeCarrinho.umCarrinho().comProduto(CriadorDeProduto.umProduto().criar()).criar())
-				.criar();
 		Pagamento pagamento = CriadorDePagamento.umPagamento().criar();
 
-		Mockito.when(this.pagamentoService.pagar(pedido)).thenReturn(pagamento);
-		Mockito.when(this.entregaService.solicitarEntrega(pedido)).thenReturn(pedidoComUidCorreios);
+		when(this.pagamentoService.pagar(any(Pedido.class))).thenReturn(pagamento);
+		when(this.entregaService.solicitarEntrega(any(Pedido.class))).thenReturn(any(Pedido.class));
 
 		thrown.expect(PagamentoNaoAutorizadoException.class);
 		thrown.expectMessage("Desculpe, seu pagamento não foi autorizado.");
 
 		this.pedidoServicePadrao.efetuarPedido(pedido);
+		
+		verify(this.pagamentoService).pagar(pedido);
+		verify(this.entregaService).solicitarEntrega(pedido);
 	}
 
 }
