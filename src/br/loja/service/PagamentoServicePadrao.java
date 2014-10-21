941f93808ca931706a7d8b6df72fe5d393e39a38
FelixCoutinho <felix.coutinho@gmail.com>
2014-10-21 12:35:06 -0300

    Exercício 13. Teste do pagamento com Argument Capture.

--- src/br/loja/service/PagamentoServicePadrao.java
+++ src/br/loja/service/PagamentoServicePadrao.java
@@ -1,12 +1,34 @@
 package br.loja.service;
 
+import java.math.BigDecimal;
+
+import br.loja.dominio.ItemPedido;
 import br.loja.dominio.Pedido;
+import br.loja.repositorio.RepositorioPagamentos;
+import br.loja.service.terceiros.GatewayPagamentoService;
 
 public class PagamentoServicePadrao implements PagamentoService {
-	
-	@Override
-	public void pagar(Pedido pedido) {
-		
+
+	private GatewayPagamentoService gatewayPagamentoService;
+	private RepositorioPagamentos repositorioPagamentos;
+
+	public PagamentoServicePadrao(GatewayPagamentoService gatewayPagamentoService,
+			RepositorioPagamentos repositorioPagamentos) {
+		this.gatewayPagamentoService = gatewayPagamentoService;
+		this.repositorioPagamentos = repositorioPagamentos;
 	}
 
+	@Override
+	public void pagar(Pedido pedido) {
+		BigDecimal valorTotal = BigDecimal.valueOf(0.0).setScale(2);
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			valorTotal = valorTotal.add((itemPedido.getProduto().getPreco()
+					.multiply(BigDecimal.valueOf(itemPedido.getQuantidade()).setScale(2)).add(itemPedido.getProduto()
+					.getValorFrete())));
+		}
+		if (this.gatewayPagamentoService.processarPagamento(valorTotal, null, null)) {
+			pedido.getPagamento().setAutorizado(true);
+		}
+		this.repositorioPagamentos.salvar(pedido.getPagamento());
+	}
 }
--- testes/br/loja/dominio/databuilders/CriadorDePedido.java
+++ testes/br/loja/dominio/databuilders/CriadorDePedido.java
@@ -1,13 +1,17 @@
 package br.loja.dominio.databuilders;
 
 import br.loja.dominio.Carrinho;
+import br.loja.dominio.Pagamento;
 import br.loja.dominio.Pedido;
+import br.loja.dominio.TipoPagamento;
 
 public class CriadorDePedido {
 
 	private String uidCorreios = "RS01902911BR";
 
 	private Carrinho carrinho = new Carrinho();
+
+	private TipoPagamento tipoPagamento;
 
 	private CriadorDePedido() {
 	}
@@ -19,6 +23,9 @@ public class CriadorDePedido {
 	public Pedido criar() {
 		Pedido pedido = new Pedido(this.carrinho);
 		pedido.setUidCorreios(uidCorreios);
+		if(tipoPagamento != null){
+			pedido.setPagamento(new Pagamento(pedido, this.tipoPagamento));
+		}
 		return pedido;
 	}
 
@@ -29,6 +36,11 @@ public class CriadorDePedido {
 
 	public CriadorDePedido comCarrinho(Carrinho carrinho) {
 		this.carrinho = carrinho;
+		return this;
+	}
+
+	public CriadorDePedido comPagamento(TipoPagamento tipoPagamento) {
+		this.tipoPagamento = tipoPagamento;
 		return this;
 	}
 
