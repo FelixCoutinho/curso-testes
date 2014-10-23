561dd253462aa0bbde9c987f0f0d99b16a86cd5d
FelixCoutinho <felix.coutinho@gmail.com>
2014-10-23 11:03:46 -0300

    Configurando o log corretamente.

--- testes/br/loja/integracao/pedidos/TestesIntegracaoPedido.java
+++ testes/br/loja/integracao/pedidos/TestesIntegracaoPedido.java
@@ -16,13 +16,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import br.loja.repositorio.RepositorioPedidos;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/applicationContext.xml" })
 @Transactional
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class, TransactionalTestExecutionListener.class })
 public class TestesIntegracaoPedido {
 
 	private static final String DATASOURCE_FILE = "br/loja/integracao/datasets/pedidos/deveriaRetornarTodosOsPedidos.xml";
