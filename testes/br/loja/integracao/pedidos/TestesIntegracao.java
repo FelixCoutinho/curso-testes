b4889f98b10e93ea8f8fc2728b983aaa89febfe7
FelixCoutinho <felix.coutinho@gmail.com>
2014-10-27 08:41:57 -0300

    Adicionando testes a suite.

---
+++ testes/br/loja/integracao/pedidos/TestesIntegracao.java
@@ -0,0 +1,11 @@
+package br.loja.integracao.pedidos;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestesIntegracaoPedido.class })
+public class TestesIntegracao {
+
+}
